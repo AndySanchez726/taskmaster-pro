c62cb266c8ca7c088ab7858ea833bd70d39e4161
Andy Sanchez <andysanchez726@gmail.com>
2020-04-16 16:26:13 -0500

    added drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,7 +234,20 @@ $(".card .list-group").sortable({
       // update array on tasks object and save
       tasks[arrName] = tempArr;
       saveTasks();
-
   }
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event,ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event,ui) {
+    console.log("out");
+  }
+});
